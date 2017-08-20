7c83f58f3e514f760ab296daed0f5ee3f8590b0d
Vladimir <dosandkv@gmail.com>
2017-08-20 23:15:45 +0300

    Step-11: creating & deleting list elements;

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class List extends React.Component {
-  render () {
-    const arr = new Array(10).fill(1).map((item, i) => i++);
-
-    return (
-      <div>
-        <h1>Hello, React!</h1>
-        Render list:
-        <ul>
-          { arr.map(item => <ListItem key={item} />) }
-        </ul>
-      </div>
-    );
-  }
-}
-
-class ListItem extends React.Component {
-  render () {
-    return (
-      <li>This is list element</li>
-    );
-  }
-}
+import List from './components/list';
 
 ReactDOM.render(<List color="red" />, document.getElementById('root'));
