57f98c1adff1bdf1b71ee750e73ef7a8789ec4e1
Vladimir <dosandkv@gmail.com>
2017-08-20 21:12:10 +0300

    Step-8: rendering children via this.props.children property

--- app.js
+++ app.js
@@ -3,70 +3,29 @@ console.error('React', React);
 const rootElement = document.getElementById('root');
 
 class ControlledComponent extends React.Component {
-  constructor (...args) {
-    super(...args);
-
-    this.state = {
-      value: ''
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleSubmit(event) {
-    console.error(`A name was submitted: ${this.state.value}`);
-    event.preventDefault();
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
   render () {
-    console.error('render');
     return (
       <div>
         <h1>Hello, React!</h1>
         <div className="description" style={{ color: this.props.color }}>
-          This is controlled component
+          This components renders child components
         </div>
-        <form onSubmit={this.handleSubmit}>
-          <input value={this.state.value} onChange={this.handleChange} type="text"/>
-          <input type="submit"/>
-        </form>
-        <div id="container" className="container"/>
+        { this.props.children }
       </div>
     );
   }
 }
 
 class UncontrolledComponent extends React.Component {
-  constructor(...args) {
-    super(...args);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    console.error(`A name was submitted: ${this.input.value}`);
-    event.preventDefault();
-  }
-
   render () {
     return (
-      <div>
-        <div>This is uncontrolled component</div>
-        <form onSubmit={this.handleSubmit}>
-          <input defaultValue="some value" type="text" ref={el => this.input = el} />
-          <input type="submit"/>
-        </form>
-      </div>
+      <div>This is child component</div>
     );
   }
 }
 
-ReactDOM.render(<ControlledComponent color="red" />, rootElement);
-
-const container = document.getElementById('container');
-
-ReactDOM.render(<UncontrolledComponent />, container);
+ReactDOM.render(
+  <ControlledComponent color="red" >
+    <UncontrolledComponent />
+  </ControlledComponent>,
+rootElement);
