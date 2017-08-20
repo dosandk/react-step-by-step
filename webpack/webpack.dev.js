2315cac9cdf232f31fceb4ea30ae4505ef59c3ec
Vladimir <dosandkv@gmail.com>
2017-08-20 23:27:26 +0300

    Step-12: added loaders for scss;

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -27,6 +27,25 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+            query: {
+              modules: true,
+              camelCase: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
       }
     ]
   },
